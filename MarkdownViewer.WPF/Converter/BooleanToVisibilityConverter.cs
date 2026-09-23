ffe5307df04a236b95a3cfa01d29d19788689a36
Andrea Ianni <andreaianni@hotmail.com>
2026-09-23 15:40:54 +0200

    #dev added a converter to convert bool to visibility with the boolean inversion

---
+++ MarkdownViewer.WPF/Converter/BooleanToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace MarkdownViewer.WPF.Converter
+{
+	[ValueConversion(typeof(bool), typeof(Visibility))]
+	internal class BooleanToVisibilityConverter : IValueConverter
+	{
+		public bool InvertSource { get; set; } = false;
+		public bool UseHidden { get; set; } = false;
+
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (value is bool source)
+			{
+				if (source ^ InvertSource)
+					return Visibility.Visible;
+				else
+					return UseHidden ? Visibility.Hidden : Visibility.Collapsed;
+			}
+			return DependencyProperty.UnsetValue;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+			=> Binding.DoNothing;
+	}
+}
