241d4eb937925d9e335554428f3b4ac90ed05a5f
Andrea Ianni <andreaianni@hotmail.com>
2026-09-23 14:30:45 +0200

    #dev now the software can load and show a MD file

--- MarkdownViewer.WPF/ViewModel/MainWindowViewModel.cs
+++ MarkdownViewer.WPF/ViewModel/MainWindowViewModel.cs
@@ -11,12 +11,43 @@ namespace MarkdownViewer.WPF.ViewModel
     internal partial class MainWindowViewModel : ObservableObject
     {
 		[ObservableProperty]
+		private string filename = "";
+
+		[ObservableProperty]
 		private string markdownText = "";
 
-		[RelayCommand]
+		[ObservableProperty]
+		private bool canOpenFile = true;
+
+		[ObservableProperty]
+		private bool isLoadingAFile = false;
+
+		[RelayCommand(CanExecute = nameof(CanOpenFile))]
 		private async Task OpenFile()
 		{
+			CanOpenFile = false;
 
+			//TODO replace with a interface for testing purpuse
+			var dialog = new Microsoft.Win32.OpenFileDialog()
+			{
+				AddToRecent = true,
+				CheckFileExists = true,
+				Multiselect = false,
+				Title = "Select a MD file",
+			};
+
+			if (dialog.ShowDialog() == true)
+			{
+				IsLoadingAFile = true;
+				var fileName = dialog.FileName;
+
+				MarkdownText = await System.IO.File.ReadAllTextAsync(fileName);
+				Filename = System.IO.Path.GetFileName(fileName);
+
+				IsLoadingAFile = false;
+			}
+
+			CanOpenFile = true;
 		}
 	}
 }
