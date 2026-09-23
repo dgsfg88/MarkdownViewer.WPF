a52161ddfad72407618d51cf1086585270757e77
Andrea Ianni <andreaianni@hotmail.com>
2026-09-23 15:54:37 +0200

    #dev added Save and Save as features

--- MarkdownViewer.WPF/ViewModel/MainWindowViewModel.cs
+++ MarkdownViewer.WPF/ViewModel/MainWindowViewModel.cs
@@ -10,6 +10,8 @@ namespace MarkdownViewer.WPF.ViewModel
 {
     internal partial class MainWindowViewModel : ObservableObject
     {
+		private string fullPath = "";
+
 		[ObservableProperty]
 		private string filename = "";
 
@@ -24,6 +26,47 @@ namespace MarkdownViewer.WPF.ViewModel
 
 		[ObservableProperty]
 		private bool showText = false;
+
+		[ObservableProperty]
+		private bool canSaveFile = true;
+
+		[RelayCommand(CanExecute = nameof(CanSaveFile))]
+		private async Task SaveAs()
+		{
+			CanSaveFile = false;
+
+			//TODO replace with a interface for testing purpuse
+			var dialog = new Microsoft.Win32.OpenFileDialog()
+			{
+				AddToRecent = true,
+				CheckFileExists = false,
+				Multiselect = false,
+				DefaultExt = ".md",
+				FileName = fullPath,
+				AddExtension = true,
+				Title = "Choose where save the MD file",
+			};
+
+			if (dialog.ShowDialog() == true)
+			{
+				fullPath = dialog.FileName;
+				Filename = System.IO.Path.GetFileName(fullPath);
+				await Save();
+			}
+		}
+
+		[RelayCommand(CanExecute = nameof(CanSaveFile))]
+		private async Task Save()
+		{
+			CanSaveFile = false;
+
+			if (string.IsNullOrWhiteSpace(fullPath))
+				await SaveAs();
+			else
+				await System.IO.File.WriteAllTextAsync(fullPath, MarkdownText);
+
+			CanSaveFile = true;
+		}
 
 		[RelayCommand(CanExecute = nameof(CanOpenFile))]
 		private async Task OpenFile()
@@ -42,10 +85,10 @@ namespace MarkdownViewer.WPF.ViewModel
 			if (dialog.ShowDialog() == true)
 			{
 				IsLoadingAFile = true;
-				var fileName = dialog.FileName;
+				fullPath = dialog.FileName;
 
-				MarkdownText = await System.IO.File.ReadAllTextAsync(fileName);
-				Filename = System.IO.Path.GetFileName(fileName);
+				MarkdownText = await System.IO.File.ReadAllTextAsync(fullPath);
+				Filename = System.IO.Path.GetFileName(fullPath);
 
 				IsLoadingAFile = false;
 			}
